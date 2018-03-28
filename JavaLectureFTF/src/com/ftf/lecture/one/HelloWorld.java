aa30076a06ca6f20c1293e8f0ba9fadecfda65f2
Asadul Haque <haq.asad000@gmail.com>
2018-03-28 04:29:06 -0400

    Initial Commit
    Creating master branch with project JavaLectureFTF. The project is
    created with HelloWorld class.

---
+++ JavaLectureFTF/src/com/ftf/lecture/one/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.ftf.lecture.one;
+
+//==================================================================
+//		Subject:	First Java Program / Hello World Program
+//		Author:		Asadul Haque
+//		Date:		03/01/2018
+//==================================================================
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
