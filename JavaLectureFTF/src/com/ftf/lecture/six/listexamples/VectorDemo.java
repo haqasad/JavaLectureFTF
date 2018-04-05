f03476109e5383d45eb6738ccbc37878ac3ad404
haqasad <haq.asad000@gmail.com>
2018-04-05 16:32:50 -0400

    Added lecture six: list example
    Lecture six is added with two classes: 
    ArrayListDemo, VectorDemo

---
+++ JavaLectureFTF/src/com/ftf/lecture/six/listexamples/VectorDemo.java
@@ -0,0 +1,46 @@
+package com.ftf.lecture.six.listexamples;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		List<String> emperors = new Vector<>();
+		
+		emperors.add("Babur");
+		emperors.add("Humayun");
+		emperors.add("Akbar");
+		emperors.add("Jahangir");
+		emperors.add("Saha Jahan");
+		emperors.add("Aurangzeb");
+		
+		System.out.println("Name of Emperors : " + emperors);
+		System.out.println("Total Emperors: " + emperors.size());
+		
+		String secondEmperors = emperors.get(1);
+		System.out.println("Second emperor's name: " + secondEmperors);
+		
+		emperors.set(2, "Akbar the Great");
+		System.out.println("Name of Emperors : " + emperors);
+		
+//		remove 4th elements
+		emperors.remove(3);
+		
+//		remove Aurangzeb
+		emperors.remove("Aurangzeb");
+		System.out.println("After removing 2 elements : " + emperors); 
+		
+//		Add duplicates
+		emperors.add("Shah Jahan");
+		System.out.println("After adding a new element : " + emperors); 
+		
+		int indexOfHumayun = emperors.indexOf("Humayun");
+		System.out.println("Index of Humayun : " + indexOfHumayun); 
+		
+		int lastIndexOfShahJahan = emperors.lastIndexOf("Shah Jahan");
+		System.out.println("Last index of Shah Jahan: " + lastIndexOfShahJahan);		
+
+	}
+
+}
