ab0612242f5726b784378b62ae5bf3d3cbb3fe1f
haqasad <haq.asad000@gmail.com>
2018-04-05 16:45:01 -0400

    Added lecture six: set example
    Lecture six is added with one classes:
    HashSetExample, LinkedHashSetExample, TreeSetExample

---
+++ JavaLectureFTF/src/com/examples/six/setexamples/HashSetExample.java
@@ -0,0 +1,22 @@
+package com.examples.six.setexamples;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+
+	public static void main(String[] args) {
+		Set<String> cities = new HashSet<>();
+		
+		cities.add("Dhaka");
+		cities.add("New York");
+		cities.add("Kolkata");
+		cities.add("Tokyo"); 
+		cities.add("London");
+		cities.add("Tokyo");
+		
+		System.out.println("Total cities: " + cities.size()); 
+		System.out.println(cities); 
+	}
+
+}
---
+++ JavaLectureFTF/src/com/examples/six/setexamples/LinkedHashSetExample.java
@@ -0,0 +1,22 @@
+package com.examples.six.setexamples;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetExample {
+
+	public static void main(String[] args) {
+		
+		Set<String> cities = new LinkedHashSet<>();
+		
+		cities.add("Dhaka");
+		cities.add("New York");
+		cities.add("Kolkata");
+		cities.add("Tokyo"); 
+		cities.add("London");
+		cities.add("Tokyo");
+		
+		System.out.println(cities); 		
+	}
+
+}
