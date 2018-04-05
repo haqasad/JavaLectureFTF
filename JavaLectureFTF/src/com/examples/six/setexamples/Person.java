48859509abb1d9fc22486284740db9a6cb213191
haqasad <haq.asad000@gmail.com>
2018-04-05 16:48:41 -0400

    Added lecture six: set example
    Lecture six is added with one class:
    Person

---
+++ JavaLectureFTF/src/com/examples/six/setexamples/Person.java
@@ -0,0 +1,20 @@
+package com.examples.six.setexamples;
+
+public class Person implements Comparable<Person> {
+	private String name;
+
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	@Override
+	public int compareTo(Person o) {
+		return name.compareTo(o.name);
+	}
+
+}
